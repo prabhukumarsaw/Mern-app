7aa08b03e790c2aec51cf4cb462cecb59ff6dd8f
Alpha <ajayvish21099@gmail.com>
2023-04-22 11:54:02 +0530

    he

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const User = require('./model/userSchema');
 
 app.use(require('./router/auth'));
 
-const PORT =process.env.PORT;
+const PORT =process.env.PORT || 5000;
 
 //middelware
 
@@ -26,6 +26,10 @@ app.get('/', (req,res) => {
 app.get('/about',middelware, (req,res) => {
     res.send('I am Ready to connect the server!!');
 });
+
+if(process.env.NODE_ENV = "production") {
+    app.use(express.static("client/build"))
+}
 
 app.listen(PORT, () => {
     console.log('server is running at port no %s', PORT);
